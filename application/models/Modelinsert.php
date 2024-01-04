ce0d079772b659386392ee6a14f9f8f558a7d833
zhiaam <zhiabanjarmasin55@gmail.com>
2024-01-05 04:18:13 +0700

    std_absen ditiadakan

--- application/models/Modelinsert.php
+++ application/models/Modelinsert.php
@@ -5,19 +5,26 @@ class Modelinsert extends CI_Model {
 
 	function absen($data)
 	{
-		$insert = $this->db->insert_batch('std_absen', $data);
+        $insert = $this->db->insert_batch('std_rekap_absen', $data);
 
-		$this->db->where('abs_ket', '');
-		$selec = $this->db->delete('std_absen');
-		
-		$copy = $this->db->query('INSERT INTO std_rekap_absen(abs_nisn,abs_cl_code,abs_ket,abs_ket_lain,abs_date,abs_tg,abs_bln,abs_th,abs_tp,abs_semester) SELECT abs_nisn,abs_cl_code,abs_ket,abs_ket_lain,abs_date,abs_tg,abs_bln,abs_th,abs_tp,abs_semester FROM std_absen ');
-		$kosong = $this->db->truncate('std_absen');
-
-		if ($insert && $selec && $copy && $kosong) {
+        if ($insert) {
 			return TRUE;
 		} else {
 			return FALSE;
 		}
+		// $insert = $this->db->insert_batch('std_absen', $data);
+
+		// $this->db->where('abs_ket', '');
+		// $selec = $this->db->delete('std_absen');
+		
+		// $copy = $this->db->query('INSERT INTO std_rekap_absen(abs_nisn,abs_cl_code,abs_ket,abs_ket_lain,abs_date,abs_tg,abs_bln,abs_th,abs_tp,abs_semester) SELECT abs_nisn,abs_cl_code,abs_ket,abs_ket_lain,abs_date,abs_tg,abs_bln,abs_th,abs_tp,abs_semester FROM std_absen ');
+		// $kosong = $this->db->truncate('std_absen');
+
+		// if ($insert && $selec && $copy && $kosong) {
+		// 	return TRUE;
+		// } else {
+		// 	return FALSE;
+		// }
 
 
 	}	
